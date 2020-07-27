c1d0993e289b21e446df4f58a7b5fad7cf112be5
weidehai <243395655@qq.com>
2020-07-28 00:16:27 +0800

    security pw

--- config/DBConfig.py
+++ config/DBConfig.py
@@ -1,8 +1,8 @@
 DB_HOST = 'localhost'
-DB_USERNAME = 'root'
-DB_PASSWORD = 'weidehai123'
-DB_DBNAME = 'myblog'
-DB_PORT = 3306
+DB_USERNAME = 'xxxxxxxx'
+DB_PASSWORD = 'xxxxxxxx'
+DB_DBNAME = 'xxxxxxxxxx'
+DB_PORT = xxxxxxxxxx
 
 DB_MIN_CACHED = 10
 DB_MAX_CACHED = 10
