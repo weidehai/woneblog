b3ef48a34374cdf1d12ae400471d5d63ccbd9aa6
weidehai <243395655@qq.com>
2021-04-29 21:15:35 +0800

    update .gitignore

--- config/__init__.py
+++
@@ -1,6 +0,0 @@
-flask_config={
-    'SQLALCHEMY_DATABASE_URI': 'mysql+pymysql://woneread@localhost:3306/woneblog',
-    'SQLALCHEMY_TRACK_MODIFICATIONS':True,
-    'SECRET_KEY': b'd#\xa1\x80\xea]*~\xd6"\xe1\x98\xb47\xfa\x90',
-    "UPLOAD_FOLDER":'E:/data/woneblog_dev/static/upload/'
-}
